cb027f17297c8b8671b9af1061a5abbb4af6cff3
vdnuit <97578112+vdnuit@users.noreply.github.com>
2022-11-23 14:41:17 +0900

    ts

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import Router from "./Router";
 import { darkTheme, lightTheme } from "./theme";
 import { useRecoilValue } from "recoil";
 import { isDarkAtom } from "./atoms";
-// import reset from "styled-reset";
-
-const GlobalStyle = createGlobalStyle`
-
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
-  // font import
-
- `;
+import reset from "styled-reset";
 
 function App() {
   const isDark = useRecoilValue(isDarkAtom);
@@ -23,5 +16,10 @@ function App() {
     </>
   );
 }
+const GlobalStyle = createGlobalStyle`
+${reset}
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+  // font import
 
+ `;
 export default App;
