1ed431a42af2a7930e71fa8ffd0509bf19c916d3
vdnuit <97578112+vdnuit@users.noreply.github.com>
2022-11-23 15:01:59 +0900

    ts

--- src/App.js
+++ src/App.js
@@ -2,13 +2,6 @@ import { createGlobalStyle } from "styled-components";
 import Router from "./Router";
 import reset from "styled-reset";
 
-const GlobalStyle = createGlobalStyle`
-${reset}
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
-  // font import
-
- `;
-
 function App() {
   return (
     <>
@@ -17,5 +10,10 @@ function App() {
     </>
   );
 }
+const GlobalStyle = createGlobalStyle`
+${reset}
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+  // font import
 
+ `;
 export default App;
