c53a4ba94ba15c64914c47c37fd6ce3f283aa0a8
vdnuit <97578112+vdnuit@users.noreply.github.com>
2022-11-23 15:08:32 +0900

    ts

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
 }
 const GlobalStyle = createGlobalStyle`
 ${reset}
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
   // font import
 
  `;
