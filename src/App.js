40df886dd548f9b2eb1ee7cff4ea52988372f9b9
vdnuit <97578112+vdnuit@users.noreply.github.com>
2022-11-26 01:47:12 +0900

    최종

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ import reset from "styled-reset";
 import Footer from "./components/Footer";
 import NoMobile from "./components/NoMobile";
 import InterTTF from "./assets/Inter.ttf";
+import Helmet from "react-helmet";
+import CookieImg from "./assets/CookieImg.png";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>내가 사는 쿠키</title>
+        <meta
+          name="description"
+          content="네이버 웹툰 쿠키 계산 서비스인 <내가 사는 쿠키>입니다."
+        />
+
+        <link rel="icon" href={CookieImg} />
+      </Helmet>
       <NoMobile />
       <Router />
       <GlobalStyle />
